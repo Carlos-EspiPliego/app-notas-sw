df4548f7098c31ccc90d480e179ff9d22ca0523b
AlessandroGSC <alessandrocgs2@gmail.com>
2023-10-05 22:36:33 -0600

    style: ocultar opciones y cambiar color de card con nota terminada

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -4,23 +4,33 @@ import { DeleteFilled, CheckCircleFilled } from '@ant-design/icons';
 
 export default function NoteCard({note}) {
     return (
-        <div className='note-card'>
+        <div className={note.noteDone ? "note-done-card" : "note-card"}>
             <div className='note-detail'>
                 <h3 className='note-title'>{note.note}</h3>
             </div>
             <div className='note-options'>
-                    <Button 
-                        shape="circle" 
-                        icon={<CheckCircleFilled />} 
-                        className="icon-done" 
-                        onClick={() => console.log("PRESS DONE")}
-                    />
-                    <Button 
-                        shape="circle" 
-                        icon={<DeleteFilled />} 
-                        className="icon-delete" 
-                        onClick={() => console.log("PRESS DELETE")}
-                    />
+                {note.noteDone !== true && (
+                    <div>
+                        <Button 
+                            shape="circle" 
+                            icon={<CheckCircleFilled />} 
+                            className="icon-done" 
+                            onClick={() => console.log("PRESS DONE")}
+                        />
+                        <Button 
+                            shape="circle" 
+                            icon={<DeleteFilled />} 
+                            className="icon-delete" 
+                            onClick={() => console.log("PRESS DELETE")}
+                        />
+                    </div>
+                )}
+                {note.noteDone && (
+                    <div>
+                        <CheckCircleFilled className='icon-note-done' />
+                    </div>
+                )}
+                    
             </div>
         </div>
     )

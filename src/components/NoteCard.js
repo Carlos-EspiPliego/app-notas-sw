1a2c44fdb2a278e0994271659c9dd24e285211bd
AlessandroGSC <alessandrocgs2@gmail.com>
2023-10-05 22:18:11 -0600

    style: componentes para mostrar notas

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Button } from 'antd'
+import { DeleteFilled, CheckCircleFilled } from '@ant-design/icons';
+
 export default function NoteCard({note}) {
     return (
         <div className='note-card'>
-            {note.note}
+            <div className='note-detail'>
+                <h3 className='note-title'>{note.note}</h3>
+            </div>
+            <div className='note-options'>
+                    <Button 
+                        shape="circle" 
+                        icon={<CheckCircleFilled />} 
+                        className="icon-done" 
+                        onClick={() => console.log("PRESS DONE")}
+                    />
+                    <Button 
+                        shape="circle" 
+                        icon={<DeleteFilled />} 
+                        className="icon-delete" 
+                        onClick={() => console.log("PRESS DELETE")}
+                    />
+            </div>
         </div>
     )
 }
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -3,9 +3,17 @@ import NoteCard from './NoteCard'
 import '../styles/notes.css';
 
 export default function NotesList({notes}) {
+
+    // Ordenamos array de notas de manera descendente por id
+    const sortedNotes = notes.sort((a, b) => {
+        const idA = parseInt(a.id);
+        const idB = parseInt(b.id);
+        return idB - idA;
+    });
+
     return (
         <div className='container-notes-list'>
-            {notes.map((nt) => (
+            {sortedNotes.map((nt) => (
                 <NoteCard note={nt}/>
             ))}
         </div>
